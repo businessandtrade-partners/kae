f80b761026dfa163c9071e0b5a2fcdfae639314d
Ammaar Limbada <alimbada@users.noreply.github.com>
2025-02-26 16:15:32 +0000

    reverted and additional config for aws-sdk

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,12 @@
 require "carrierwave"
 require "carrierwave/storage/fog"
 require "fog/aws"
+require 'aws-sdk-s3'
+
+Aws.config.update({
+  region: ENV["AWS_REGION"],
+  credentials: Aws::ECSCredentials.new
+})
 
 class CustomStorage
   def self.new(uploader)
